1909a468bad2ec7208d67f0b05f6859e23745d68
patriciabernardos <ptrbernardos@gmail.com>
2025-06-06 14:15:39 +0200

    chore: configura CORS en servidor backend para permitir peticiones desde frontend

--- src/server.js
+++ src/server.js
@@ -8,7 +8,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(
+ {
+  origin: ['http://localhost:5173' /*'https://tu-dominio.com'*/]
+} 
+));
 app.use(express.json());
 
 // Rutas
